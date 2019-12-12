f5240d30a0a27d617ff1526c282a29ea40090c47
juliancastaeda <1277@holbertonschool.com>
2019-12-12 14:55:24 -0500

    upload commit

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import add_0
-a = 1
-b = 2
-c = a + b
-print('{:d} + {:d} = {:d}'.format(a, b, c))
+from add_0 import add
+if __name__ == '__main__':
+    a = 1
+    b = 2
+    print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
