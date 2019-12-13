dd2d99761fdcb4ce8726a16c100ac9d3480103c0
juliancastaeda <1277@holbertonschool.com>
2019-12-12 21:46:31 -0500

    upload point 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,10 @@ import sys
 if __name__ == "__main__":
     if len(sys.argv) == 0:
         print("{} arguments.".format(len(sys.argv) - 1))
-    elif len(sys.argv) >= 1:
+    elif len(sys.argv) > 1:
         print("{} arguments:".format(len(sys.argv) - 1))
         for i in range(1, len(sys.argv)):
             print("{}: {}".format((i), sys.argv[i]))
+    elif len(sys.argv) == 1:
+        print("{} arguments:".format(len(sys.argv) - 1))
+        print("{}: {}".format((i), sys.argv[i]))        
