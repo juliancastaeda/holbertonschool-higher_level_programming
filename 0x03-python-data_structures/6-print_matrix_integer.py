ac47c9943acdfedfe0ea10d0e29be30dbc505096
juliancastaeda <1277@holbertonschool.com>
2019-12-17 18:11:26 -0500

    load point 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for k in range(len(matrix)):
+        for j in range(len(matrix[k])):
+            if (j == len(matrix[k])) - 1:
+                print('{:d}'.format(matrix[k][j]), end='')
+            else:
+                print('{:d}'.format(matrix[k][j]), end=' ')
+        print()
