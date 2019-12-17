3370a4139890c8bbea1623bfd4faa1451f7ccba0
juliancastaeda <1277@holbertonschool.com>
2019-12-17 13:10:12 -0500

    load point 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,10 +3,7 @@ def element_at(my_list, idx):
     for i in range(0, len(my_list)):
         if (idx < 0):
             return (None)
-        if (idx > 4):
+        if (idx > len(my_list)):
             return (None)
         else:
             return (my_list[idx])
-       
-        
-        
