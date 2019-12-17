c2e1e83717c0f29e7749e53e74da66bf65f614b6
juliancastaeda <1277@holbertonschool.com>
2019-12-17 18:17:49 -0500

    upload point 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -6,4 +6,4 @@ def print_matrix_integer(matrix=[[]]):
                 print('{:d}'.format(matrix[k][j]), end='')
             else:
                 print('{:d}'.format(matrix[k][j]), end='')
-        print()        
+        print()
