54d580c19461e2da1e72bc21588ef07e66fcaab8
juliancastaeda <1277@holbertonschool.com>
2019-12-17 13:27:39 -0500

    upload point 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    siz = len(my_list) 
-    if idx < 0 or idx > siz:
-        return (None)
+    siz = len(my_list)
+    if (idx < 0) or (idx > siz):
+        return None
     else:
-        return (my_list[idx])
+        return my_list[idx]
