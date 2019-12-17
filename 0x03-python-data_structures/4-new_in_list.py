047c6b904a982dd61e5adb71e7dc973ed20eaa15
juliancastaeda <1277@holbertonschool.com>
2019-12-17 15:13:51 -0500

    load point 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    new_list= my_list[0:]
+    new_list = my_list[0:]
     if (idx < 0) or (idx >= len(my_list)):
         return (my_list)
     else:
