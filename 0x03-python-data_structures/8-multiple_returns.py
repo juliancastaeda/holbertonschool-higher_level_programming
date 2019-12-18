e747cae308574077aad4fd3713a8a833edb28a7c
juliancastaeda <1277@holbertonschool.com>
2019-12-18 13:14:48 -0500

    upload point 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    if (len == '\0'):
+    if (len == 0):
         tuple_a = (length, None)
         return (tuple_a)
     else:
-        tuple_b = (length, sentence[0])
-        return (tuple_b)
+        tuple_a = (length, sentence[0])
+        return (tuple_a)
