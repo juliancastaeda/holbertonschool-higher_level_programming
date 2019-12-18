ae8b42e71e36b44207eaad55c0daa7d87ed15b28
juliancastaeda <1277@holbertonschool.com>
2019-12-17 22:10:46 -0500

    load point

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     num_larger = my_list[0]
-    if (my_list == ""):
+    if (my_list == "\0"):
         return (None)
     for i in my_list:
         if num_larger <= i:
             num_larger = i
     return (num_larger)
-            
-    
-
-
-    
