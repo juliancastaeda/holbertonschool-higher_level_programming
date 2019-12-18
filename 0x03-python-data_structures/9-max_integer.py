b79562ac3865a82a0022c1adad433ec592e7c01d
juliancastaeda <1277@holbertonschool.com>
2019-12-18 13:28:05 -0500

    upload point 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,7 +3,7 @@ def max_integer(my_list=[]):
     num_larger = my_list[0]
     if (len(my_list) == 0):
         return (None)
-    for i in my_list:
-        if num_larger <= i:
-            num_larger = i
+    for i in range(len(my_list)):
+        if num_larger < my_list[i]:
+            num_larger = my_list[i]
     return (num_larger)
