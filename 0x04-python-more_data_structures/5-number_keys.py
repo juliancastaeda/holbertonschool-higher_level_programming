00dcd087b25b66a3fd57338aa88fc0cfe083799c
juliancastaeda <1277@holbertonschool.com>
2019-12-19 15:37:53 -0500

    upload point 5

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
-    list(enumerate(a_dictionary))
-    for i in range(len(a_dictionary)):
-        i += 1
-    return i
+    return (len(a_dictionary))
