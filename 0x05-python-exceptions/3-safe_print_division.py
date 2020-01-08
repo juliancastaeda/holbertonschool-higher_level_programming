8a06ef209fcc4c303898f39e9c7e8d6f3857e309
juliancastaeda <1277@holbertonschool.com>
2020-01-08 18:04:32 -0500

    upload point3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
+    c = 'Inside result:'
     try:
         if b != 0:
             return a/b
-    finally:
-        print("Inside result:")
-        if b == 0:
+        else:
             return None
+    except TypeError:
+        print()
+    finally:
+        print("{}".format(c)) 
