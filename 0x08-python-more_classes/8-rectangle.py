55f8a7d413f54a9aa11af89ad33f53ee7dad6fbe
juliancastaeda <1277@holbertonschool.com>
2020-01-14 23:09:53 -0500

    upload points 7 and 8 and 9

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -55,7 +55,9 @@ class Rectangle():
         if self.__width == 0 and self.__height == 0:
             print()
         else:
-            return (((((str(self.print_symbol * self.__width)) + '\n') * (self.__height)))[:-1])
+            my_list = ((str(self.print_symbol * self.__width)) + '\n')
+            my_list_1 = ((my_list * (self.__height))[:-1])
+            return my_list_1
 
     def __repr__(self):
         return("Rectangle ({:d}, {:d})".format(self.__width, self.__height))
