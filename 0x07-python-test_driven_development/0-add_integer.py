7b1c832abea0af0f79a09a40f4cf5b8abe0b6857
juliancastaeda <1277@holbertonschool.com>
2020-01-14 23:25:55 -0500

    upload oint 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -4,6 +4,8 @@
    b = integuer
    return add a + b
 """
+
+
 def add_integer(a, b=98):
     """
        this function
@@ -14,8 +16,8 @@ def add_integer(a, b=98):
         raise TypeError("b must be an integer")
     if type(a) is int or type(a) is float:
         if type(a) is float:
-           a = int(a)
+            a = int(a)
     if type(b) is int or type(b) is float:
         if type(b) is float:
-          b = int(b)
+            b = int(b)
     return a + b
