408616a2145eae94f7d810d4881f5fe8d0e12b78
juliancastaeda <1277@holbertonschool.com>
2020-01-21 11:08:54 -0500

    upload point 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+
+
 def inherits_from(obj, a_class):
     if type(obj) is not a_class:
-        if isinstance(obj, a_class) is True:
+        a = type(obj)
+        if issubclass(a, a_class) is True:
             return True
     else:
         return False
