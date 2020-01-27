2ee109b1136bc1ea457eb92c9bcdc47eaa043903
juliancastaeda <1277@holbertonschool.com>
2020-01-27 16:27:14 -0500

    load point

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -12,7 +12,7 @@ comment
 
 class Rectangle(Base):
     """Class Rectangle"""
-    
+
     def __init__(self, width, height, x=0, y=0, id=None):
         """function coments"""
         super().__init__(id)
