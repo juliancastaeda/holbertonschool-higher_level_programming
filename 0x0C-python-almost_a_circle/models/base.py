413480d24478a47defb0f3b6971cb9ef6b8ffbd2
juliancastaeda <1277@holbertonschool.com>
2020-01-27 14:12:47 -0500

    load file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -7,16 +7,11 @@ comment
 class Base:
     """Class Base"""
     __nb_objects = 0
-    
+
     def __init__(self, id=None):
         """function comments"""
-        if id != None: 
+        if id is not None:
             self.id = id
         else:
-           Base.__nb_objects += 1
-           self.id = Base.__nb_objects
-           
-        
-        
-    
-    
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
