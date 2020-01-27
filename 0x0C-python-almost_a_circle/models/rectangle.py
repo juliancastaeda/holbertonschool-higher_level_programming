60ccd7c91060336e7fe448ce16944b8c3574df51
juliancastaeda <1277@holbertonschool.com>
2020-01-27 17:14:54 -0500

    load point

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -16,7 +16,7 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         """function coments"""
         super().__init__(id)
-         if type(width) is not int:
+        if type(width) is not int:
             raise TypeError('width must be an integer')
         if width <= 0:
             raise ValueError('width must be > 0')
@@ -24,11 +24,11 @@ class Rectangle(Base):
             raise TypeError('height must be an integer')
         if height <= 0:
             raise ValueError('height must be > 0')
-         if type(x) is not int:
+        if type(x) is not int:
             raise TypeError('x must be an integer')
         if x < 0:
             raise ValueError('x must be >= 0')
-         if type(y) is not int:
+        if type(y) is not int:
             raise TypeError('y must be an integer')
         if y < 0:
             raise ValueError('y must be >= 0')
