dc209d13b50dabc50eb1d6cbd2aecd9800e67f9d
juliancastaeda <1277@holbertonschool.com>
2020-01-27 15:43:47 -0500

    load file

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -12,15 +12,14 @@ comment
 
 class Rectangle(Base):
     """Class Rectangle"""
-    __nb_objects = 0
-
+    
     def __init__(self, width, height, x=0, y=0, id=None):
         """function coments"""
         super().__init__(id)
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
 
     @property
     def width(self):
