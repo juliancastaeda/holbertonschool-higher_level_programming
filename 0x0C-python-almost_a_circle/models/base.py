0398e078b7d9ff350f14b993ede61bd30d5f0f6e
juliancastaeda <1277@holbertonschool.com>
2020-01-28 09:57:32 -0500

    upload file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -25,4 +25,5 @@ class Base:
         """
         if list_dictionaries is None:
             return "[]"
-        return json.dumps(list_dictionaries)
+        a = json.dumps(list_dictionaries)
+        return a
