fd2f5f723c63ee1876bd0cb79b218c5e877f4656
juliancastaeda <1277@holbertonschool.com>
2020-01-28 09:49:05 -0500

    upload file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,6 +4,9 @@ comment
 """
 
 
+import json
+
+
 class Base:
     """Class Base"""
     __nb_objects = 0
@@ -15,3 +18,12 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """
+        comment
+        """
+        if len(list_dictionaries) < 0 and list_dictionaries is None:
+            return '"[]"'
+        else:
+            return json.dumps(list_dictionaries)
