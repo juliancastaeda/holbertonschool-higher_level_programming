01e3cac243e21c60d7559263e047c9e368b81227
juliancastaeda <1277@holbertonschool.com>
2020-01-29 15:05:50 -0500

    upload file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -43,7 +43,7 @@ class Base:
 
     @staticmethod
     def from_json_string(json_string):
-        if json_string is None or json_string is '':
+        if json_string is None or len(json_string) == 0:
             return '[]'
         else:
             return json.loads(json_string)
