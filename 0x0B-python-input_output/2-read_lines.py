4d641732833493835b30161b7a54637b8e48a54d
juliancastaeda <1277@holbertonschool.com>
2020-02-05 19:07:27 -0500

    upload point

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def read_lines(filename="", nb_lines=0):
     j = 0
-    with open('my_file_0.txt') as filename:
-        for i in filename:
+    with open(filename) as file:
+        for i in file:
             j += 1
         if nb_lines <= 0 or nb_lines >= j:
-            filename.seek(0)
-            print(filename.read(), end='')
+            file.seek(0)
+            print(file.read(), end='')
         else:
-            filename.seek(0)
+            file.seek(0)
             for i in range(nb_lines,):
-                print(filename.readline(), end='')
+                print(file.readline(), end='')
