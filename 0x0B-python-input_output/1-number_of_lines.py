3bb3a441e1cd028c08d2057f114bff7504bba736
juliancastaeda <1277@holbertonschool.com>
2020-02-05 19:03:59 -0500

    upload file

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def number_of_lines(filename=""):
-    filename = open('my_file_0.txt', 'r')
-    a = len(filename.readlines())
-    return a
-    filename.close
+    with open(filename, 'r') as file:
+        a = 0
+        for num in file:
+            a += 1
+        return a
