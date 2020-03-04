5023ff30ecba53f9596e0bed83dcbf8ecd1750e1
juliancastaeda <1277@holbertonschool.com>
2020-03-03 21:09:26 -0500

    load file

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,5 @@
+--
+--
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE cities (id INT NOT NULL AUTO_INCREMENT, state_id INT NOT NULL, PRIMARY KEY(id), FOREIGN KEY(state_id) REFERENCES states(id), name VARCHAR(256));
