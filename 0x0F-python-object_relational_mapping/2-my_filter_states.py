b74b4f0d020614b3b00999b2a9de901843e719cc
juliancastaeda <1277@holbertonschool.com>
2020-03-29 09:23:34 -0500

    upload file task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     username = sys.argv[1]
     password = sys.argv[2]
     dbname = sys.argv[3]
-    stateNameSearched = sys.argv[4]
+    state = sys.argv[4]
     connection = MySQLdb.Connect(host="localhost",
                                  port=3306,
                                  user=username,
@@ -21,7 +21,7 @@ if __name__ == '__main__':
     cur.execute('''
                 SELECT * FROM states
                 WHERE states.name = '{}' ORDER BY id ASC
-                '''.format(stateNameSearched))
+                '''.format(state))
     row = cur.fetchall()
     for row in row:
         print(row)
