5faf5abbde03e941669c449f9f97bcda1566cc5b
juliancastaeda <1277@holbertonschool.com>
2020-03-29 00:23:23 -0500

    uplod file

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -14,13 +14,13 @@ if __name__ == "__main__":
         database = sys.argv[3]
         state = sys.argv[4]
     connection = MySQLdb.connect(host='localhost', port=3306, user=username,
-                         passwd=password, db=database, charset="utf8")
+                                 passwd=password, db=database, charset="utf8")
     cur = connection.cursor()
     cur.execute('''SELECT name FROM cities WHERE state_id=(SELECT
                     id FROM states WHERE name='{}')'''.format(state))
     city = ""
     for i in cur.fetchall():
-        name_city = city + i[0] + ", "
+        city = city + i[0] + ", "
     print(city[0:-2])
     cur.close()
     connection.close()
