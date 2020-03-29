6954dcf6294b532ef4f2f78f34324d6ced8741e2
juliancastaeda <1277@holbertonschool.com>
2020-03-28 20:10:04 -0500

    upload file

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
+"""
+
+
+"""
 import sys
 import MySQLdb
 
-if __name__ == "__main__":
-    user=sys.argv[1],
-    passwd=sys.argv[2],
-    db=sys.argv[3],
-    state=sys.argvargv[4]
-    conn = MySQLdb.connect(host="localhost",
-                           port=3306,
-                           user=username,
-                           passwd=password,
-                           db=dbname,
-    cur = conn.cursor()
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbname = sys.argv[3]
+    state = sys.argv[4]
+    connection = MySQLdb.Connect(host="localhost",
+                                 port=3306,
+                                 user=username,
+                                 passwd=password,
+                                 db=dbname,
+                                 charset="utf8")
+    cur = connection.cursor()
     cur.execute('''
-                SELECT * FROM states
-                WHERE states.name = '{}' ORDER BY id ASC
-                '''.format(state))
-    query_rows = cur.fetchall()
-    for row in query_rows:
+                        SELECT * FROM states
+                        WHERE states.name = '{}' ORDER BY id ASC
+                       '''.format(state))
+    qR = cur.fetchall()
+    for row in qR:
         print(row)
-    cur.close()
-    conn.close()
