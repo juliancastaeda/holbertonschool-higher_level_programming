fe11c52e4891fbbfb42803d79c2577495f8d2a97
juliancastaeda <1277@holbertonschool.com>
2020-05-05 17:24:53 -0500

    upload file 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
-let num = parseInt(process.argv[2]);
+const num = parseInt(process.argv[2]);
 if (isNaN(num)) {
   console.log('1');
 } else {
   let fact = 1;
   for (let count = num; count > 0; count--) {
-      fact *= count;
+    fact *= count;
   }
-    console.log(fact);
+  console.log(fact);
 }
