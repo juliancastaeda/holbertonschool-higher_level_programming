0a8861e0ef5bb5c32839a1ec11d247308f248113
juliancastaeda <1277@holbertonschool.com>
2020-05-11 21:41:36 -0500

    upload file task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,14 +1,11 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
-class Square extends Rectangle {
-    constructor (size) {
-	super (size, size);
+const square = require('./5-square');
+class Square extends square {
+  charPrint (c) {
+    if (c === undefined) { c = 'X'; }
+    for (let count = 0; count < this.height; count++) {
+      console.log(c.repeat(this.width));
     }
-    charPrint(c) {
-	if (c === undefined) {c = 'X';}
-	for (let count = 0; count < this.height; count++) {
-	    console.log(c.repeat(this.width));
-	}
-    }
+  }
 }
 module.exports = Square;
